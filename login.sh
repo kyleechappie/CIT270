6310e17ad895b23517dd73d13fc47b1d0b9d73e0
kylee chapman <chappie1@byui.edu>
2023-05-05 13:25:04 -0600

    johnny boi

--- login.sh
+++ login.sh
@@ -1,1 +1,5 @@
 echo "Logging in"
+
+curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
+
+curl -v https://dev.stedi.me/validate/61fdf251-b885-438a-8b7b-e85934884838
