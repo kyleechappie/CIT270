5bd56db36460969676a299bb21b58ca560be675e
kylee chapman <chappie1@byui.edu>
2023-06-14 13:06:51 -0600

    turtle that is a ninja

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const fs = require('fs')
 https.createServer({
   key: fs.readFileSync('/etc/letsencrypt/archive/kyleechapman.cit270.com/privkey1.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/archive/kyleechapman.cit270.com/cert1.pem'),
-  chain: fs.readFileSync('/etc/letsencrypt/archive/kyleechapman.cit270.com/fullchain1.pem')
+  ca: fs.readFileSync('/etc/letsencrypt/archive/kyleechapman.cit270.com/chain1.pem')
 }, app).listen(port, () => {
   redisClient.connect(); 
   console.log('Listening...')
